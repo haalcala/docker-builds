86e8f0809a19a080aaa7a22214369b0c85c1ad68
Harold Alcala <harold.dev@gmail.com>
2023-01-06 20:49:11 +0800

    prometheus and test app

--- test-app-1/main.go
+++ test-app-1/main.go
@@ -15,20 +15,36 @@ func recordMetrics() {
 	go func() {
 		for {
 			opsProcessed.Inc()
+
+			totalPluginApiDuration.WithLabelValues("pluginId123", "apiName_aaaa", "success_true").Observe(float64(time.Now().Minute()))
+
 			time.Sleep(5 * time.Second)
 		}
 	}()
 }
 
 var (
+	namespace = "myapp"
+
 	opsProcessed = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
+		Namespace: namespace,
+		Subsystem: "processed",
+		Name:      "ops_total",
+		Help:      "The total number of processed events",
 	})
+
+	totalPluginApiDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: "api",
+		Name:      "plugin_time",
+	}, []string{"pluginID", "apiName", "success"})
 )
 
 func init() {
-	prometheus.Register((opsProcessed))
+	prometheus.MustRegister(
+		opsProcessed,
+		totalPluginApiDuration,
+	)
 }
 
 func main() {
@@ -39,11 +55,11 @@ func main() {
 
 	router := mux.NewRouter()
 
-	// Serving static files
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-
 	// Prometheus endpoint
 	router.Path("/metrics").Handler(promhttp.Handler())
+
+	// Serving static files
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
 	fmt.Println("Serving requests on port 2112")
 	err := http.ListenAndServe(":2112", router)
