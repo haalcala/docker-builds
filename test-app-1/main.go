47396ffbceb4463cbb736ffbb1db75c0aea06541
Harold Alcala <harold.dev@gmail.com>
2023-01-06 16:12:57 +0800

    test app

---
+++ test-app-1/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(5 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func init() {
+	prometheus.Register((opsProcessed))
+}
+
+func main() {
+	recordMetrics()
+
+	// http.Handle("/metrics", promhttp.Handler())
+	// http.ListenAndServe(":2112", nil)
+
+	router := mux.NewRouter()
+
+	// Serving static files
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+
+	// Prometheus endpoint
+	router.Path("/metrics").Handler(promhttp.Handler())
+
+	fmt.Println("Serving requests on port 9000")
+	err := http.ListenAndServe(":9000", router)
+	log.Fatal(err)
+}
