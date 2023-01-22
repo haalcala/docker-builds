115920c8e7b7e4378bd17f2a3aeac8c608e95fae
Harold Alcala <harold.dev@gmail.com>
2023-01-23 02:43:55 +0800

    jenkins

---
+++ jenkins/build/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set +x
+
+[ ! -e "$HOME/.ssh" ] && mkdir $HOME/.ssh
+
+cd $HOME/.ssh
+
+if [ ! -e "id_rsa" ]; then
+    ssh-keygen -t rsa -P "" -f $HOME/.ssh/id_rsa -C "The access key for Jenkins slaves"
+
+    cat id_rsa.pub >> authorized_keys
+fi
+
+export PATH="$PATH:/opt/java/openjdk/bin"
+export JAVA_HOME="/opt/java/openjdk/bin"
+
+echo PATH: $PATH
+
+# sleep Infinity
+
+bash -l /usr/local/bin/jenkins.sh
