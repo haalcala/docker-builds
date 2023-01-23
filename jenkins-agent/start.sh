8054f6b5252f1d0c7fdb6d9831cb16e9ae3d9aed
Harold Alcala <harold.dev@gmail.com>
2023-01-23 11:21:16 +0800

    jenkins

--- jenkins-agent/start.sh
+++ jenkins-agent/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/usr/sbin/sshd -D &
+/usr/sbin/sshd -D
 
-curl -sO http://jenkins-master:8080/jnlpJars/agent.jar
+# curl -sO http://jenkins-master:8080/jnlpJars/agent.jar
 
-java -jar agent.jar -jnlpUrl http://jenkins-master:8080/manage/computer/$JENKINS_AGENT_NAME/jenkins-agent.jnlp -secret $JENKINS_MASTER_SECRET -workDir "/home/jenkins"
+# java -jar agent.jar -jnlpUrl http://jenkins-master:8080/manage/computer/$JENKINS_AGENT_NAME/jenkins-agent.jnlp -secret $JENKINS_MASTER_SECRET -workDir "/home/jenkins"
