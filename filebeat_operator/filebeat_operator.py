e744d66b6d768f2c2bcc68ca076a47af9ed14842
Harold Alcala <harold.dev@gmail.com>
2023-02-14 12:50:48 +0800

    real_json

--- filebeat_operator/filebeat_operator.py
+++ filebeat_operator/filebeat_operator.py
@@ -9,6 +9,8 @@ import json
 import os
 import copy
 
+import real_json
+
 import subprocess
 
 from time import sleep
@@ -68,7 +70,7 @@ def filter_targetgroups(targetgroups, filter):
                 if targetgroup[key] != value:
                     include = False
                     break
-            elif isinstance(value, (GetWrappedJson, dict)) and 'regex' in value:
+            elif isinstance(value, (real_json.ify, dict)) and 'regex' in value:
                 regex = value['regex']
                 if not re.match(regex, targetgroup[key]):
                     include = False
@@ -80,7 +82,7 @@ def filter_targetgroups(targetgroups, filter):
 def main(args):
     config = None
     with open(("test-" if args.dev else "") + 'config.yml') as file:
-        config = GetWrappedJson(yaml.load(file, Loader=yaml.FullLoader))
+        config = real_json.ify(yaml.load(file, Loader=yaml.FullLoader))
         # print("config:", config)
 
         # sort_file = yaml.dump(config.filebeat.base_config, sort_keys=True)
@@ -120,12 +122,12 @@ def main(args):
 
         try:
             with open(output_file) as file:
-                existing_config = GetWrappedJson(yaml.load(file, Loader=yaml.FullLoader))
+                existing_config = real_json.ify(yaml.load(file, Loader=yaml.FullLoader))
                 # print("existing_config:", existing_config)
         except Exception as e:
             print(e)
 
-        new_config = GetWrappedJson(copy.deepcopy(config.filebeat.base_config.__dict__["_data"]))
+        new_config = real_json.ify(copy.deepcopy(config.filebeat.base_config.__dict__["_data"]))
 
         # print("----------------------------\nnew_config:",new_config,"\n--------------------------")
         # print("----------------------------\nnew_config['output.elasticsearch'].hosts:",new_config['output.elasticsearch'].hosts,"\n--------------------------")
@@ -133,7 +135,7 @@ def main(args):
         elb = session.client('elbv2', region)
         ec2 = session.client('ec2', region)
 
-        tgs = GetWrappedJson(elb.describe_target_groups())
+        tgs = real_json.ify(elb.describe_target_groups())
         tgs = filter_targetgroups(tgs.TargetGroups, config.aws.target.resources[0].conditions[0])
 
         TargetGroupArns = []
@@ -152,7 +154,7 @@ def main(args):
         tmp_config: Config = None
 
         # interpret the tags
-        for tags in GetWrappedJson(elb.describe_tags(ResourceArns=TargetGroupArns)).TagDescriptions:
+        for tags in real_json.ify(elb.describe_tags(ResourceArns=TargetGroupArns)).TagDescriptions:
             # print("-----------------------------")
             # print("tags:", tags)
 
@@ -162,12 +164,12 @@ def main(args):
 
             # get the host and port
             if tmp_config and tmp_config.enabled:
-                health = GetWrappedJson(elb.describe_target_health(TargetGroupArn=tags.ResourceArn))
+                health = real_json.ify(elb.describe_target_health(TargetGroupArn=tags.ResourceArn))
 
                 # print("health:", health)
 
                 for target in health.TargetHealthDescriptions:
-                    instances = GetWrappedJson(ec2.describe_instances(InstanceIds=[target.Target.Id]))
+                    instances = real_json.ify(ec2.describe_instances(InstanceIds=[target.Target.Id]))
 
                     for instance in instances.Reservations[0].Instances:
 
@@ -230,93 +232,6 @@ def main(args):
         the_thread.start()
 
         the_thread.join()
-
-class GetWrappedJson:
-    def __init__(self, data):
-        self._data = data
-    
-    def __getattr__(self, name):
-        # print(f"Getting attribute {name} {type(self._data)}")
-
-        if isinstance(self._data, dict) and name in self._data:
-            value = self._data[name]
-            if isinstance(value, (dict, list)):
-                return GetWrappedJson(value)
-            return value
-        elif isinstance(self._data, dict):
-            if name in dir(dict):
-                return getattr(self._data, name)
-            return None
-        elif isinstance(self._data, list):
-            if name == "append":
-                def wrapped_append(kw):
-                    if isinstance(kw, GetWrappedJson):
-                        self._data.append(kw._data)
-                    else:
-                        self._data.append(kw)
-                return wrapped_append
-            if name in dir(list):
-                return getattr(self._data, name)
-            return None
-        return None
-
-    def __getitem__(self, name):
-        # print(f"Getting item {name} {type(self._data)}")
-        if isinstance(self._data, list) and isinstance(name, int) and 0 <= name < len(self._data):
-            value = self._data[name]
-            if isinstance(value, (dict, list)):
-                return GetWrappedJson(value)
-            return value
-        elif isinstance(self._data, dict) and name in self._data:
-            value = self._data[name]
-            if isinstance(value, (dict, list)):
-                return GetWrappedJson(value)
-            return value
-        return None
-
-    def __setattr__(self, name, value):
-        if name != "_data":
-            if isinstance(self._data, dict):
-                if isinstance(value, GetWrappedJson):
-                    self._data[name] = value._data
-                else:
-                    self._data[name] = value
-        else:
-            super().__setattr__(name, value)
-
-    def __setitem__(self, key, value):
-        if isinstance(self._data, list) and isinstance(key, int) and 0 <= key < len(self._data):
-            if isinstance(value, GetWrappedJson):
-                self._data[key] = value._data
-            else:
-                self._data[key] = value
-        elif isinstance(self._data, dict):
-            if isinstance(value, GetWrappedJson):
-                self._data[key] = value._data
-            else:
-                self._data[key] = value
-
-    def __len__(self):
-        return len(self._data)
-
-    def __iter__(self):
-        for value in self._data:
-            if isinstance(value, (dict, list)):
-                yield GetWrappedJson(value)
-            else:
-                yield value
-
-
-    def __str__(self):
-        return str(self._data)
-
-    def __repr__(self):
-        return repr(self._data)
-
-
-    def __bool__(self):
-        return bool(self._data)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
